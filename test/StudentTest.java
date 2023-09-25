933861a19819f7040593f2d02cd1560c5cb4bc05
Tianyu Chen <chentianyu@outlook.com>
2023-09-25 16:29:47 -0400

    boilerplate for student tests

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 import java.util.Random;
+
 import static java.lang.Math.log;
 
 public class StudentTest {
@@ -10,7 +12,7 @@ public class StudentTest {
     @Test
     public void insertSmallBST() {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>((Integer x, Integer y) -> x < y);
-        TreeMap<Integer,Integer> map = new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         int[] a = new int[]{4, 8, 0, 2, 6, 10};
         /*
          *       4
@@ -24,9 +26,18 @@ public class StudentTest {
             bst.insert(key);
             map.put(key, key);
         }
-	for (int i = 0; i != 11; ++i) {
+        for (int i = 0; i != 11; ++i) {
             assertEquals(bst.contains(i), map.containsKey(i));
-	}
+        }
     }
-    
+
+    /**
+     * TODO: Test cases
+     */
+    @Test
+    public void test() {
+        insertSmallBST();
+        // your tests go here
+    }
+
 }
